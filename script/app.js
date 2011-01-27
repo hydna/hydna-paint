5908b9e1070282bdafa83af0b004cfb33e50f9ed
Gustaf Sjöberg <gs@distrop.com>
2011-01-27 14:33:05 +0100

    auto-detect transport.

--- script/app.js
+++ script/app.js
@@ -53,9 +53,7 @@ $(document).ready(function() {
     var canvas = document.querySelector('canvas');
 
     // open a stream to hydna in read/write mode
-    var stream = new HydnaStream('flash.hydna.net:7010', 'rw', null, {
-        transport: 'ws',
-    });
+    var stream = new HydnaStream('flash.hydna.net:7010', 'rw');
 
     // draw figure when data is received over stream
     stream.onmessage = function(data) {
