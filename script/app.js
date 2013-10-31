3037bb85803dd6bcae6b4439ce2c361f21b8d214
Johan Dahlberg <dahlberg.johan@gmail.com>
2013-10-31 10:27:27 +0100

    Support for multi-touch

--- script/app.js
+++ script/app.js
@@ -1,127 +1,243 @@
-var paint = {
-    id: 0,
-    canvas: null,
-    context: null,
-    channel: null,
-    drawing: false,
-    s_color: '#e53a3c',
-    s_size: 2,
-    prev_x: 0,
-    prev_y: 0,
-    sizes: {
-        thin: 2,
-        medium: 4,
-        thick: 8
-    },
-    init: function(canvas, channel) {
-        paint.id = paint.uid();
-        paint.canvas = canvas;
-        paint.channel = channel;
-        paint.context = canvas.get(0).getContext('2d');
-        canvas.bind('mousedown touchstart', paint.mousedown);
-        canvas.bind('mouseup touchend', paint.mouseup);
-        canvas.bind('mousemove touchmove', paint.mousemove);
-        canvas.get(0).onselectstart = function() {
-            // prevent cursor from changing.
-            return false;
-        };
-        $(window).bind('resize', function(event) {
-            canvas.get(0).width = canvas.parent().width();
-            canvas.get(0).height = canvas.parent().height();
-        });
-        $(window).resize();
-    },
-    mousedown: function(event) {
-        paint.drawing = true;
-        paint.prev_x = event.clientX;
-        paint.prev_y = event.clientY - 40;
-        return false;
-    },
-    mouseup: function(event) {
-        paint.drawing = false;
-        return false;
-    },
-    mousemove: function(event) {
-        var e = (
-            window.event && window.event.targetTouches
-            ?window.event.targetTouches[0]
-            :event
-        );
-        if (paint.drawing) {
-            var position = paint.canvas.parent().position();
-            var new_x = e.clientX;
-            var new_y = e.clientY - 40;
-            
-            var graph = {
-                x: new_x,
-                y: new_y,
-                px: paint.prev_x,
-                py: paint.prev_y,
-                w: paint.s_size,
-                c: paint.s_color,
-                id: paint.id
-            };
-            
-            paint.draw(graph.px, graph.py, graph.x, graph.y, graph.w, graph.c);
-            
-            paint.channel.send(JSON.stringify(graph));
-            
-            paint.prev_x = new_x;
-            paint.prev_y = new_y;
-        }
-        return false;
-    },
-    draw: function(px, py, x, y, width, color) {
-        
-        paint.context.strokeStyle = color;
-        paint.context.beginPath();
-        
-        paint.context.moveTo(px, py);
-        paint.context.lineTo(x, y);
-        
-        paint.context.lineWidth = width;
-        paint.context.lineCap = 'round';
-        paint.context.strokeStyle = color;
-        paint.context.stroke();
-    },
-    
-    uid: function(){
-        
-        var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-        var uuid = [], i;
-        var len = 4;
-        var radix = 16;
-        radix = radix || chars.length;
-        
-        for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
-        
-        return uuid.join('');
+(function (global) {
+'use strict';
+
+var URL                       = 'simple-paint.hydna.net';
+
+var BRUSH_WIDTH               = 8;
+
+var TOUCH_DEVICE              = 'ontouchstart' in global ||
+                                'onmsgesturechange' in global;
+
+
+(function setupController () {
+  var userColor = '#e53a3c';
+  var input;
+  var channel;
+  var viewport;
+  var elem
+
+  function stateHandler (connected, reason) {
+    input.enabled = connected;
+    console.log("user id: %s", channel.userId);
+    console.log("user count: %s", channel.userCount);
+  }
+
+  function dataHandler (data) {
+    if ('c' in data == false) {
+      data.c = userColor;
+      channel.send(data);
     }
-};
+    viewport.draw(data);
+  }
 
-$(document).ready(function() {
-    var canvas = $('canvas');
+  elem = document.getElementById('canvas');
+  viewport = new CanvasViewport(elem);
 
-    // open a stream to hydna in read/write mode
-    var channel = new HydnaChannel('simple-paint.hydna.net/1111', 'rw');
+  input = TOUCH_DEVICE ? new TouchInterface(elem)
+                       : new PointerInterface(elem);
 
-    // draw figure when data is received over channel
-    channel.onmessage = function(event) {
-        var graph = JSON.parse(event.data);
-        if(graph.id != paint.id){
-            paint.draw(graph.px, graph.py, graph.x, graph.y, graph.w, graph.c);
+  input.ondata = dataHandler;
+
+  channel = new PaintChannel(URL);
+
+  channel.ondata = dataHandler;
+  channel.onstate = stateHandler;
+}());
+
+
+
+function PaintChannel (url) {
+  var self = this;
+  var connected = false;
+  var channel;
+  var userid;
+
+  this.userId = null;
+  this.userCount = 0;
+
+  this.send = function (data) {
+    data.id = self.userId;
+    channel.send(JSON.stringify(data), 2);
+  };
+
+  (function setup () {
+    channel = new HydnaChannel(url, 'rw');
+
+    channel.onopen = function (event) {
+      self.userId = event.data.split(',')[0];
+      self.userCount = parseInt(event.data.split(',')[1]);
+      connected = true;
+      self.onstate(connected);
+    };
+
+    channel.onsignal = function (event) {
+      self.userCount = parseInt(event.data);
+    }
+
+    channel.onmessage = function (event) {
+      try {
+        var data = JSON.parse(event.data);
+        if (data.id != userid) {
+          self.ondata(data);
         }
+      } catch (encodingError) {
+      }
     };
 
-    // initiate paint when channel is ready.
-    channel.onopen = function() {
-        paint.init(canvas, channel);
+    channel.onclose = function (event) {
+      self.userId = null;
+      self.userCount = 0;
+      connected = false;
+      self.onstate(connected, event.reason);
+      if (event.hadError) {
+        return setTimeout(setup, 3000);
+      }
     };
+  }());
+}
 
-    $('.picker li').click(function(event) {
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-        paint.s_color = $('#color li.active').text();
-        paint.s_size = paint.sizes[$('#stroke li.active').text().toLowerCase()];
-    });
-});
+
+function TouchInterface (target) {
+  var self = this;
+  var moves = null;
+
+  this.enabled = false;
+
+  function relativePos (t) {
+    return { x: t.pageX - t.target.parentNode.offsetLeft,
+             y: t.pageY - t.target.parentNode.offsetTop };
+  }
+
+  target.addEventListener('touchstart', function (event) {
+    var touch;
+
+    if (self.enabled == false) {
+      return;
+    }
+
+    event.preventDefault();
+
+    moves = moves || {};
+
+    for (var i = 0; i < event.changedTouches.length; i++) {
+      touch = event.changedTouches[i];
+      moves[touch.identifier] = relativePos(touch);
+    }
+  });
+
+  target.addEventListener('touchmove', function (event) {
+    var touch;
+    var data;
+    var move;
+    var pos;
+
+    if (!moves || self.enabled == false) {
+      return;
+    }
+
+    event.preventDefault();
+
+    for (var i = 0; i < event.changedTouches.length; i++) {
+      touch = event.changedTouches[i];
+
+      if (!(move = moves[touch.identifier])) {
+        continue;
+      }
+
+      pos = relativePos(touch);
+      data = { x: pos.x, y: pos.y, px: move.x, py: move.y };
+
+      self.ondata(data);
+
+      moves[touch.identifier] = pos;
+    }
+  });
+
+  target.addEventListener('touchend', function (event) {
+    var touch;
+
+    if (!moves) {
+      return;
+    }
+
+    event.preventDefault();
+
+    for (var i = 0; i < event.changedTouches.length; i++) {
+      touch = event.changedTouches[i];
+      if (touch.identifier in moves) {
+        delete moves[touch.identifier];
+      }
+    }
+
+    if (Object.keys(moves) == 0) {
+      moves = null;
+    }
+  });
+}
+
+
+function PointerInterface (target) {
+  var self = this;
+  var state = null;
+
+  this.enabled = false;
+
+  function handler (name, callback) {
+    if (target.attachEvent) {
+      target.attachEvent('on' + name, callback);
+    } else {
+      target.addEventListener(name, callback);
+    }
+  }
+
+  handler('mousedown', function (event) {
+    if (self.enabled == false) {
+      return;
+    }
+
+    state = { x: event.offsetX, y: event.offestY };
+
+    return false;
+  });
+
+  handler('mousemove', function (event) {
+    var data;
+
+    if (!state || self.enabled == false) {
+      return;
+    }
+
+    data = { x: event.offsetX, y: event.offsetY, px: state.x, py: state.y };
+
+    self.ondata(data);
+
+    state = { x: event.offsetX, y: event.offsetY };
+
+    return false;
+  });
+
+  handler('mouseup', function (event) {
+    state = null;
+    return false;
+  });
+}
+
+
+function CanvasViewport (target) {
+  var context = target.getContext('2d');
+
+  target.onselectstart = function() { return false; };
+
+  this.draw = function (data) {
+    context.strokeStyle = data.c;
+    context.beginPath();
+    context.moveTo(data.px, data.py);
+    context.lineTo(data.x, data.y);
+    context.lineWidth = BRUSH_WIDTH;
+    context.lineCap = 'round';
+    context.stroke();
+  };
+}
+
+
+}(this));
